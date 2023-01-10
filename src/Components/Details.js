dbc1ef7d36b2f811effedb64c8440b57bb9ec6d8
Beth Wilson <bethwilson777@gmail.com>
2023-01-10 10:23:22 -0700

    Added additional info to details page and button styling for article link

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -9,6 +9,8 @@ const Details = ({article}) => {
         <div className='details-component'>
             <img src={article[0].multimedia[1].url} className='details-image'></img>
             <h1 className='details-title'>{article[0].title}</h1>
+            <p className='details-abstract'>{article[0].abstract}</p>
+            <a href={article[0].url} target="_blank" className='button-35'>Full Article</a>
         </div>
     )
 }
