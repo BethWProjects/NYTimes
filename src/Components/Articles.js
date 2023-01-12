10f105a01226a2ea8c290fdbfc0c49555f9e90f6
Beth Wilson <bethwilson777@gmail.com>
2023-01-11 18:01:17 -0700

    Fixed filter logic

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -4,9 +4,15 @@ import Card from "./Card"
 
  //filter over articles data and assign key value pairs to display on the Card component
 
-const Articles = ({articles}) => {
+const Articles = ({articles, filteredCategory}) => {
+    let data = articles
+    if (filteredCategory.length >= 1) {
+        data = filteredCategory
+    } else {
+        data = articles
+    }
 
-    const articleData = articles.map((article) => {
+    const articleData = data.map((article) => {
         return(
             <Card 
                 id={article.published_date}
